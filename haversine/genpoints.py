087b17fa03918d9041964f39b85375dd55109187
Chancy Kennedy <chancy@conciergecloset.com>
2023-02-28 10:11:29 -0600

    more like lat long

--- haversine/genpoints.py
+++ haversine/genpoints.py
@@ -1,11 +1,24 @@
 import json
 import random
 
-# generate a list of 10 million random point pairs
-pairs = []
-for i in range(10000000):
-    pairs.append({'x0': random.random(), 'y0': random.random(), 'x1': random.random(), 'y1': random.random()})
 
-# write it to a JSON file
-with open('data_10000000_flex.json', 'w') as f:
-    json.dump({'pairs': pairs}, f)
+def rand_sign():
+    if random.randrange(2) == 0:
+        return -1
+    else:
+        return 1
+
+
+def rand_value():
+    return random.randrange(100000000) * rand_sign() / 1000000
+
+
+if __name__ == '__main__':
+    # generate a list of 10 million random point pairs
+    pairs = []
+    for i in range(10000000):
+        pairs.append({'x0': rand_value(), 'y0': rand_value(), 'x1': rand_value(), 'y1': rand_value()})
+
+    # write it to a JSON file
+    with open('data_10000000_flex.json', 'w') as f:
+        json.dump({'pairs': pairs}, f, indent=2)
